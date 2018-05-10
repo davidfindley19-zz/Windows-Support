91602715f6fb49ccaba0fc51a7b1106ca3df5cc5
davidfindley19 <davidfindley19@users.noreply.github.com>
2018-05-10 15:55:09 -0500

    Updated for clarification

--- AD Account Creation.ps1
+++ AD Account Creation.ps1
@@ -7,11 +7,12 @@
 .NOTES
     Author: David Findley
     Date: May 10 2018
+    Version: 1.01 - Edited for consistency with our production version.
 #>
 
 Write-Host "Manual Account and Mailbox Creation"
 
-# Importing Active Directory module for AD manipulation.
+# Importing Active Directory module for AD manipulation. 
 Import-Module ActiveDirectory -ErrorAction SilentlyContinue
 
 # Just grabbing current user credentials. This is assuming user executing script has privileges to modify domain users.
@@ -21,9 +22,10 @@ $UserCredential = Get-Credential "$env:USERDOMAIN\$env:USERNAME"
 $FirstName = Read-Host -Prompt "Enter the user's first name:"
 $LastName = Read-Host -Prompt "Enter the user's last name:"
 
-# Creating username based off of first initial and last name standard.
+# Creates all lowercase username based on first initial and last name standard.
 $UserName = $($FirstName.Substring(0,1) + $LastName).ToLower()
 
+# Defines variables used later. Most of these are hard coded as they apply to each user. Some information is made up currently :)
 $FullName = "$FirstName " + "$LastName"
 $SamAccountName = $UserName
 $GivenName = $FirstName
@@ -35,7 +37,7 @@ $State = "State"
 $Company = "Business Name"
 $PhoneNumber = "555-555-5555"
 
-# Sanity check on username creation. 
+# Sanity check on username creation. Eventually this will query AD and verify that the account doesn't exist. 
 Write-Host "The following user, $FullName, will be created using the following username: $UserName. "
 $Readhost = Read-Host "[Y]es or [N]o"
 switch ($Readhost) {
@@ -44,6 +46,7 @@ switch ($Readhost) {
     Default {"No response. Exiting script"; exit}
 }
 
+# We have to break up the commands here because of the limitations with older versions of Powershell. 
 if ($Create -eq $true) {
     try {
         New-ADUser  -Name $FullName `
@@ -64,9 +67,6 @@ else {
 }
 
 # Due to the limitations mentioned above, we'll now fill in the missing information for the created account.
-
-#$NewUser = Get-ADUser -Filter {-SamAccountName -eq $SamAccountName}
-
 if (dsquery user -samid $UserName) {
     Get-ADUser $UserName | Set-ADUser   -GivenName $FirstName `
                                         -Surname  $LastName `
@@ -82,9 +82,4 @@ else {
     Write-Host "User does not exist in AD."
     
 }
-#Write-Host "New account created with username $UserName"
-<#Write-Host  $SamAccountName `
-            $EmailAddress `
-            $Company `
-            $FullName
-            #>
+
