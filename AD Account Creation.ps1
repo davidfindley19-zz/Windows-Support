968f3f021d807f94298ea2a0c003d3b925ee9883
davidfindley19 <davidfindley19@users.noreply.github.com>
2018-05-15 11:21:38 -0500

    Added exchange support
    A few other tweaks for checking AD for username availability, etc.

--- AD Account Creation.ps1
+++ AD Account Creation.ps1
@@ -6,13 +6,12 @@
     is created, based off the role, groups/permissions will be assigned to the user. The final step of this script is to create the mailbox on our Exchange server.
 .NOTES
     Author: David Findley
-    Date: May 10 2018
-    Version: 1.3 - Multi-group support in AD. 
+    Date: May 15, 2018
 #>
 
 Write-Host "Manual Account and Mailbox Creation"
 
-# Importing Active Directory module for AD manipulation. 
+# Importing Active Directory module for AD manipulation.
 Import-Module ActiveDirectory -ErrorAction SilentlyContinue
 
 # Just grabbing current user credentials. This is assuming user executing script has privileges to modify domain users.
@@ -22,10 +21,8 @@ $UserCredential = Get-Credential "$env:USERDOMAIN\$env:USERNAME"
 $FirstName = Read-Host -Prompt "Enter the user's first name:"
 $LastName = Read-Host -Prompt "Enter the user's last name:"
 
-# Creates all lowercase username based on first initial and last name standard.
+# Creating username based off of first initial and last name standard.
 $UserName = $($FirstName.Substring(0,1) + $LastName).ToLower()
-
-# Defines variables used later. Most of these are hard coded as they apply to each user. Some information is made up currently :)
 $FullName = "$FirstName " + "$LastName"
 $SamAccountName = $UserName
 $GivenName = $FirstName
@@ -37,28 +34,21 @@ $State = "State"
 $Company = "Business Name"
 $PhoneNumber = "555-555-5555"
 $Account = (dsquery user -samid $UserName)
+$AccountPass = "password1"
+$EncryptPass = convertto-securestring $AccountPass -asplaintext -force
 
-# Sanity check on username creation. Eventually this will query AD and verify that the account doesn't exist. 
-Write-Host "The following user, $FullName, will be created using the following username: $UserName. "
-$Readhost = Read-Host "[Y]es or [N]o"
-switch ($Readhost) {
-    Y {Write-Host "Continuing with this username. "; $Create=$true }
-    N {Write-Warning "Username is incorrect."; $Create=$false }
-    Default {"No response. Exiting script"; exit}
-}
-
-# Sanity check on username creation. Eventually this will query AD and verify that the account doesn't exist.
-if ($Account -eq $null){
-Write-Warning "The following username is available: $UserName. Would you like to continue? "
-$Readhost = Read-Host "[Y]es or [N]o"
-switch ($Readhost) {
-    Y {Write-Host "Great! Continuing with this username. "; $Create=$true }
-    N {Write-Host "Let's try that again."; $Create=$false }
-    Default {"Invalid response. Exiting script"; exit}
-}
+# Sanity check on username creation. 
+if ($Account -eq $null) {
+    Write-Host "The following username is available: $UserName. Would you like to continue? "
+    $Readhost = Read-Host "[Y]es or [N]o"
+    switch ($Readhost) {
+        Y {Write-Host "Great! Continuing with this username. "; $Create = $true } # Much easier than a bunch of if/else statements
+        N {Write-Warning "Let's try that again."; $Create = $false }
+        Default {"Invalid response. Exiting script"; exit}
+    }
 }
 else {
-    Write-Error "This username, $UserName, is not available. Please try again."
+    Write-Warning "This username, $UserName, is not available. Please try again." # Right now this exits the script, but it will eventually allow manual entry of a username.
     exit
 }
 if ($Create -eq $true) {
@@ -66,6 +56,8 @@ if ($Create -eq $true) {
         New-ADUser  -Name $FullName `
                     -SamAccountName $SamAccountName `
                     -GivenName $Surname 
+        Write-Host "$FullName : Account created successfully."
+        $JobTitle = Read-Host -Prompt "What is $FirstName $LastName's job title?"
         Write-Host "$FullName : Account created successfully." 
     }
         
@@ -80,7 +72,7 @@ else {
     exit
 }
 
-# Due to the limitations mentioned above, we'll now fill in the missing information for the created account.
+# We'll now fill in the missing information for the created account.
 if (dsquery user -samid $UserName) {
     Get-ADUser $UserName | Set-ADUser   -GivenName $FirstName `
                                         -Surname  $LastName `
@@ -89,21 +81,27 @@ if (dsquery user -samid $UserName) {
                                         -UserPrincipalName "$UserName@domain.com" `
                                         -Enable $true `
                                         -DisplayName "$FirstName $LastName"
-                                    
-    
+                                        -ChangePasswordAtLogon:$false `
+                                        -Title $JobTitle `
+                                        -MobilePhone $PhoneNumber `
+                                        -PasswordNeverExpires $true                                   
 }   
 else {
     Write-Host "User does not exist in AD."
-    
-}
+ 
+# Setting user account password parameters
+Get-ADUser $UserName | Set-ADAccountPassword -NewPassword $EncryptPass 
 
-# Adding user to group(s) based off of the team they will be joining. These three groups are examples of multiple choice. 
+}
+# Adding user to group(s) based off of the team they will be joining.
 $TeamName = Read-Host "What group will $FirstName $LastName be a part of? Please enter one: Executive, Accounting, IT "
     if ($TeamName -eq "Executive") {
-        Add-ADPrincipalGroupMembership -Identity:"CN=$FirstName $LastName,CN=Users,DC=servername,DC=local" -MemberOf:"CN=Full Group Name,CN=Users,DC=servername,DC=local", "CN=Full Group Name,CN=Users,DC=servername,DC=local", `
-        "CN=Full Group Name,CN=Users,DC=servername,DC=local", "CN=Full Group Name,CN=Users,DC=servername,DC=local", "CN=Full Group Name,CN=Users,DC=servername,DC=local", `
-        "CN=Full Group Name,CN=Users,DC=servername,DC=local" -Server:"servername.domainname.local"
-        Write-Host "User, $FirstName $LastName, successfully added to GIS Groups."
+        Add-ADPrincipalGroupMembership -Identity:"CN=$FirstName $LastName,CN=Users,DC=servername,DC=local" -MemberOf:"CN=Full Gropu Name,CN=Users,DC=servername,DC=local", "CN=GIS Portal Publishers,CN=Users,DC=servername,DC=local", `
+        "CN=GIS Map Services,CN=Users,DC=servername,DC=local", "CN=GIS Map Publishers,CN=Users,DC=servername,DC=local", "CN=GIS Internal Portal Users,CN=Users,DC=servername,DC=local", `
+        "CN=GIS Foreign User Map Services,CN=Users,DC=servername,DC=local" -Server:"servername.domainname.local"
+        Write-Host "Setting $FirstName $LastName's manager." # Added here since it adds manager based on role/team you select. 
+        Set-ADUser -Identity:"CN=$FirstName $LastName,CN=Users,DC=servername,DC=local" -Manager:"CN=First Name,CN=Users,DC=servername,DC=local" -Department:"Deparment Name for user" -Server:"servername.domainname.local" #Full path for manager account.
+        Write-Host "User, $FirstName $LastName, successfully added to group."
         }
             elseif ($TeamName -eq "Accounting"){
             Add-ADPrincipalGroupMembership -Identity:"CN=$FirstName $LastName,CN=Users,DC=servername,DC=local" -MemberOf:"CN=Full Group Name,CN=Users,DC=servername,DC=local", "CN=Full Group Name,CN=Users,DC=servername,DC=local", `
@@ -122,3 +120,9 @@ $TeamName = Read-Host "What group will $FirstName $LastName be a part of? Please
     Write-Host "User not added to any groups."
     exit
     }
+
+# Enabling new user's mailbox.
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://exchange/powershell -Authentication Kerberos -Credential $UserCredential # Connecting to remote powershell session on exchange server
+Import-PSSession $Session
+New-Mailbox -name $FullName -UserPrincipalName "$UserName@email.com" -SamAccountName $SamAccountName -FirstName $FirstName -LastName $LastName # Enabling the account
+Remove-PSSession $Session # Closed. This wraps up the account creation script. 
