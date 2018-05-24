f4179bcfbc1726b728e522cd47ea83507b11499b
TWE\dfindley <dfindley@tradewindenergy.com>
2018-05-24 15:12:05 -0500

    Finished out script

--- Driver_Install.ps1
+++ Driver_Install.ps1
@@ -11,7 +11,7 @@ if($WindowsVersion.Contains("Windows 7"))
     $EtherPath = "$driverFolder\W7 x64\Ethernet"
     Start-Process -FilePath $EtherPath\RTNIC_DELL_INST.exe -Wait
     Write-Host "Installing WD-15 USB Driver for Windows 7"
-    $UsbPath = "$driverFolder\W7 x64\USB"
+    $USBPath = "$driverFolder\W7 x64\USB"
     Start-Process $UsbPath\setup.exe -Wait
     Write-Host "Installing Wd-15 Audio Driver for Windows 7"
     $AudioPath = "$driverFolder\W7 x64\Audio"
@@ -19,5 +19,15 @@ if($WindowsVersion.Contains("Windows 7"))
 }
 else {
     Write-Host "Installing WD-15 Ethernet Driver for Windows 10"
-    $driverFolder + "W10 x64\Realtek-USB-GBE-Ethernet-Controller-Driver_3XTRW_WIN_2.43.2017.505_A05_01\RTNIC_DELL_INST.exe"
-    }
+    $EtherPath = "$driverFolder\W10 x64\setup.exe "
+    Start-Process $EtherPath\setup.exe -Wait
+    Write-Host "Installing WD-15 USB Driver for Windows 10 "
+    $USBPath = "$driverFolder\W10 x64\USB"
+    Start-Process $USBPath\setup.exe -Wait
+    Write-Host "Installing WD-15 Audio Driver for Windows 10"
+    $AudioPath = "$driverFolder\W10 x64\Audio"
+    Start-Process $AudioPath\setup.exe -Wait
+}
+
+Write-Host "All drivers have been installed."
+exit 
