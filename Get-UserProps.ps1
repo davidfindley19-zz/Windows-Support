2222a2dfcdd38421bb51ee4828088cf0721f59f1
David Findley <davidfindley19@users.noreply.github.com>
2019-03-19 15:50:05 -0500

    Update Get-UserProps.ps1
    Wrote in a logging function to record search output

--- Get-UserProps.ps1
+++ Get-UserProps.ps1
@@ -9,21 +9,50 @@
     .NOTES
         Author:     David Findley
         Date:       3/14/2019
-        Version:    1.0
+        Version:    1.2
+        Change Log:
+                    1.0 (3/14) - Imported original script from Get-ADUserProperties that initially included logging.
+                    1.1 (3/14) - Wrote as function Get-UserProps for easier searching. 
+                    1.2 (3/19) - Added Write-Log function for recording all search output. 
 #>
 
 #Declaration of function that runs the script. Domain is mandatory.
 Function Get-UserProps {
 
-Param(
-    [Parameter(Mandatory=$true)]
-    [string]$Domain
-)
-#Actual function for the "GUI" that you see.
+Function Write-Log {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message,
+     
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('Information','Warning','Error')]
+        [string]$Severity = 'Information'
+    )
+     
+    [pscustomobject]@{
+        Time = (Get-Date -F g)
+        Message = $Message
+        Severity = $Severity
+        } | Export-Csv -Path "C:\Temp\Logs\LogFileUserProps.csv" -Append -NoTypeInformation
+    }
+    
+
+Function Show-Domain{
+    Clear-Host
+    Write-Host " ========== Please select your domain to connect to ========== "
+#Multiple domains to choose from in our environment.
+    Write-Host "1: Press '1' to connect to domain.com."
+    Write-Host "2: Press '2' to connect to domain.com."
+    Write-Host "Q: Press 'Q' to quit."
+}
+#Function for the menu output.
 Function Show-Menu
 {
-    cls
-    Write-Host " ================ Checking user properties on domain: $Domain =============== "
+    Clear-Host
+    Write-Host " ================ Checking user properties on domain: $domain =============== "
 
     Write-Host "1: Press '1' to search by extensionattribute2."
     Write-Host "2: Press '2' to search by email address."
@@ -34,13 +63,22 @@ Function Show-Menu
 #The AD search function.
 Function Get-Properties {
     
-        cls
+        Clear-Host
+        Write-Log -Message "Searching the $domain domain." -Severity Information
+        Write-Log -Message "User search started at $(Get-Date -Format "HH:mm")"
         $data = [System.Collections.ArrayList]@()
         $List = Get-Content "C:\Temp\Users.txt"
         $User = $null
         Foreach ($account in $List){
-            $User = Get-ADUser -Filter {$property -like $account} -Properties * -Server $Domain
-            Write-Host "Pulling" $user.DisplayName "account information." -ForegroundColor Blue
+            $User = Get-ADUser -Filter {$property -like $account} -Properties * -Server $domain
+            Write-Host "Getting" $user.DisplayName "account information." -ForegroundColor Blue
+        
+            if($user -ne $null){
+                Write-Log -Message "Successfully found $account" -Severity Information
+            }
+            elseif($user -eq $null){
+                Write-Log -Message "$account not found." -Severity Error
+            }
 
         $ItemDetails = [PSCustomObject]@{
             Name = $account
@@ -52,15 +90,25 @@ Function Get-Properties {
             Enabled = $User.Enabled
             'Password Expired' = $User.passwordexpired
             'Account Created' = $User.whenCreated
+            'Password Last Set' = $User.passwordlastset
         }
         $data.Add($ItemDetails) > $null
         }    
 #Export as CSV for easier reporting.       
 $data | Export-Csv C:\Temp\Results.csv -NoTypeInformation
+Write-Log -Message "User search completed at $(Get-Date -Format "HH:mm")"
 
 }
+
 #Allows you to the make selection. Mostly behind the scenes for Show-Menu
 do{
+    Show-Domain
+    $domainoption = Read-Host "Please make a selection."
+    switch ($domainoption) {
+        1 {$domain = 'phibred.com'}
+        2 {$domain = 'ddnet1.net'}
+        Q {return}
+    }
 
     Show-Menu
     $input = Read-Host "Please make a selection."
@@ -80,3 +128,4 @@ do{
 until ($input -eq 'q')
 
 }
+
