89b51d6488e9bf193ffc637510fb9ae9bcfc3d22
David Findley <davidfindley19@users.noreply.github.com>
2019-07-03 13:50:39 -0500

    Initial commit

---
+++ Reset-SCCM.ps1
@@ -0,0 +1,49 @@
+Function Reset-SCCM {
+
+Write-Host "SCCM Repair Process" -ForegroundColor Green
+
+Write-Host `n"Stopping Windows Management services..."
+Stop-Service -Name Winmgmt -Force
+
+Write-Host `n"Setting SCCM uninstall location." 
+Set-Location C:\Windows\ccmsetup
+
+Write-Host `n"Starting uninstall service..."
+.\ccmsetup.exe /uninstall
+
+Write-Host `n"Waiting on the uninstall process to complete..." 
+Wait-Process ccmsetup 
+Write-Host `n"Uninstall complete. Continuing with removal." -ForegroundColor Green
+
+Set-Location C:\
+
+Write-Host `n"Removing SCCM related files and folders."
+Get-ChildItem C:\Windows\CCM | Remove-Item -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item -Path C:\Windows\ccmcache -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item -Path C:\Windows\ccmsetup -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item -Path C:\Windows\SMSCFG.INI -Confirm:$false -ErrorAction SilentlyContinue
+
+Write-Host `n"Setting location for Registry key deletion..."
+Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\CCMSetup\' -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\CCM\' -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\SMS\' -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Start-Sleep 2
+
+Write-Host `n"Restarting the WMI Service..." -ForegroundColor Yellow
+Start-Service -Name Winmgmt
+Start-Sleep 2
+
+Write-Host `n"Reinstalling the SCCM Client..." -ForegroundColor Yellow
+New-PSDrive -Name "P" -PSProvider "FileSystem" -Root "\\path\to\network\share"
+Set-Location P:\
+Start-Process .\ccmsetup.exe
+Wait-Process ccmsetup
+Start-Sleep 2
+
+Write-Host `n"Script cleanup..."
+Set-Location C:\ 
+Remove-PSDrive -Name "P"
+Start-Sleep 2
+Exit
+
+}
